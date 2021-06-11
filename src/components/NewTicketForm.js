6068ab147405232a8bc399602d9eec1ece2edeca
Drew Peterson <drew.a.peterson@hotmail.com>
2021-06-11 13:44:21 -0700

    add ticket control and set visibility state with conditional rendering

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NewTicketForm(props) {
+  return (
+    <React.Fragment>
+      <h3>This is a Form</h3>
+    </React.Fragment>
+  )
+}
+
+
+NewTicketForm.propTypes = {
+
+};
+
+
+export default NewTicketForm;
